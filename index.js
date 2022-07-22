c2477eea34fbb1a781f4524239e6b28bd0ed27fa
Mahmoud GUEFRI <mamouty4@hotmail.com>
2022-07-22 19:02:07 +0100

    Complete Call to action and Footer sections

---
+++ index.js
@@ -0,0 +1,44 @@
+
+/* ------Nav bar------ */
+
+const navLinks = document.querySelector(".nav-links");
+
+function showMenu(){
+  navLinks.classList.add("showing");
+};
+
+function hideMenu(){
+  navLinks.classList.remove("showing");
+};
+
+
+ /* ------Slidshow------ */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+  
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+  
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("travaux-col");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
